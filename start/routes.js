7c32969ccd83acdc7866d9bdafadde10885061fa
naratipcover <naratipcover@gmail.com>
2021-01-06 23:54:40 +0700

    Edit routes

--- start/routes.js
+++ start/routes.js
@@ -20,20 +20,26 @@ Route.on('/').render('welcome')
 
 Route.group(() => {
 
-    // Router University
-    Route.get('/university', 'UniversityController.index')
-    Route.get('/university/:id', 'UniversityController.show')
-    Route.post('/university', 'UniversityController.store')
-    Route.put('/university/:id', 'UniversityController.update')
-    Route.patch( '/university/:id', 'UniversityController.update')
-    Route.delete('/university/:id', 'UniversityController.destroy')
+    Route.resource('university', 'UniversityController')
+    Route.resource('student', 'StudentController')
+    Route.resource('student_university', 'StudentUniversityController')
 
-    // Router Student
-    Route.get('/student','StudentController.index')
-    Route.get('/student/:id','StudentController.show')
-    Route.post('/student','StudentController.store')
-    Route.put('/student/:id','StudentController.update')
-    Route.patch('/student/:id','StudentController.update')
-    Route.delete('/student/:id','StudentController.destroy')
+
+
+    // // Router University
+    // Route.get('/university', 'UniversityController.index')
+    // Route.get('/university/:id', 'UniversityController.show')
+    // Route.post('/university', 'UniversityController.store')
+    // Route.put('/university/:id', 'UniversityController.update')
+    // Route.patch( '/university/:id', 'UniversityController.update')
+    // Route.delete('/university/:id', 'UniversityController.destroy')
+
+    // // Router Student
+    // Route.get('/student','StudentController.index')
+    // Route.get('/student/:id','StudentController.show')
+    // Route.post('/student','StudentController.store')
+    // Route.put('/student/:id','StudentController.update')
+    // Route.patch('/student/:id','StudentController.update')
+    // Route.delete('/student/:id','StudentController.destroy')
 
   }).prefix('api/v1')
