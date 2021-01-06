83268bdecc26c2e373e14293b47cca957983a677
naratipcover <naratipcover@gmail.com>
2021-01-06 23:56:30 +0700

    Edit name value

--- app/Controllers/Http/StudentController.js
+++ app/Controllers/Http/StudentController.js
@@ -1,33 +1,31 @@
 'use strict'
 
-const University = require("../../Models/University")
-
     const Student = use('App/Models/Student')
 
 class StudentController {
 
     async index () {
-        const Students = await Student.query().fetch()
-        return { status:200, error: undefined, data: Students}
+        const students = await Student.query().fetch()
+        return { status:200, error: undefined, data: students}
     }
     
     async show ({request}) {
         const { id } = request.params
-        const Students = await Student.query().where('id', id).fetch()
-        return {Student:200, error: undefined, data: Students}
+        const students = await Student.query().where('id', id).fetch()
+        return {Student:200, error: undefined, data: students}
     }
 
     async store ({request}){
         const data = request.body
-        const Students = await Student.create(data)
-        return { status: 200,error: undefined, data: Students }
+        const students = await Student.create(data)
+        return { status: 200,error: undefined, data: students }
 
     }
 
     async update ({request}) {
         const { id } = request.params
         const data = request.body
-        const Students = await Student.query().where('id', id).update(data)
+        const students = await Student.query().where('id', id).update(data)
         const Stu = await Student.query().where('id', id).fetch()
         return {status: 200, error: undefined, data: Stu}
     }
--- app/Controllers/Http/UniversityController.js
+++ app/Controllers/Http/UniversityController.js
@@ -5,28 +5,28 @@ const University = use('App/Models/University')
 class UniversityController {
 
     async index (){
-        const Universities =  await University.query().fetch()
-        return {status: 200, error: undefined, data: Universities }
+        const universities =  await University.query().fetch()
+        return {status: 200, error: undefined, data: universities }
     }
 
     async show ({request}){
         const { id } = request.params
-        const Universities = await University.query().where('id', id).fetch()
-        return {status: 200, error: undefined, data: Universities}
+        const universities = await University.query().where('id', id).fetch()
+        return {status: 200, error: undefined, data: universities}
     }
 
     async store ({request}){
         const data = request.body
-        const  Universities = await University.create(data)
-        return {status: 200, error:undefined, data: Universities}
+        const  universities = await University.create(data)
+        return {status: 200, error:undefined, data: universities}
     }
 
     async update ({request}) {
         const {id} = request.params
         const data = request.body
-        const Universities = await University.query().where('id', id).update(data)
-        const Uni = await University.query().where('id', id).fetch()
-        return {status: 200, error: undefined, data: Uni}
+        const universities = await University.query().where('id', id).update(data)
+        const uni = await University.query().where('id', id).fetch()
+        return {status: 200, error: undefined, data: uni}
     }
 
     async destroy ({request}) {
